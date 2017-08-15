5fdf50a84333a338dc8c310e028b00c30235db21
Nicolas Marfurt <nmarfurt@gmail.com>
2017-08-15 20:45:02 +0200

    Initial Commit

---
+++ ServiceContainer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServiceContainer
+//
+//  Created by Nicolas Marfurt on 15.08.17.
+//  Copyright © 2017 Nicolas Marfurt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ServiceContainerTests/ServiceContainerTests.m
@@ -0,0 +1,39 @@
+//
+//  ServiceContainerTests.m
+//  ServiceContainerTests
+//
+//  Created by Nicolas Marfurt on 15.08.17.
+//  Copyright © 2017 Nicolas Marfurt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ServiceContainerTests : XCTestCase
+
+@end
+
+@implementation ServiceContainerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
